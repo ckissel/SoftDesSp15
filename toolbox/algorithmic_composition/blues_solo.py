eb098031b3004580ab85830064e4e443c39ff152
Caleb Kissel <caleb.kissel@students.olin.edu>
2015-04-03 00:42:25 -0400

    music composition push

--- toolbox/algorithmic_composition/blues_solo.py
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -31,6 +31,19 @@ solo = AudioStream(sampling_rate, 1)
 blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
 beats_per_minute = 45				# Let's make a slow blues solo
 
-add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+# add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+
+curr_note = 0
+add_note(solo, bass, blues_scale[curr_note], 1.0, beats_per_minute, 1.0)
+
+licks = [ [ [1,0.52], [1,0.48], [1, 0.49], [1, 0.51] ],[ [-1, 0.35], [-1, 0.15], [-1, 0.15], [-1, 0.35] ],[ [-1, 0.25], [1, 0.25], [-1, 0.25], [1, 0.25] ] ]
+for i in range(4):
+    lick = choice(licks)
+    if curr_note < 0 or curr_note > len(blues_scale)-1:
+        break
+    for note in lick:
+        curr_note += note[0]
+        add_note(solo, bass, blues_scale[curr_note], note[1], beats_per_minute, 1.0)
+
 
 solo >> "blues_solo.wav"
